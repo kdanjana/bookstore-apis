ecf5a31a5fc62dc0d86dee24e1202bfef724f30b
anjana <kad.an.devi@gmail.com>
2023-10-29 13:52:00 +0530

    added static root

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +27,8 @@ urlpatterns = [
     path("accounts/", include('user_accounts.urls')),
     #path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += ( re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), )
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 
 python manage.py makemigrations
 python manage.py migrate
-gunicorn todoapp.wsgi:application -c gunicorn_config.py --log-level=debug --access-logfile=- --error-logfile=-
+gunicorn bookstore.wsgi:application -c gunicorn_config.py --log-level=debug --access-logfile=- --error-logfile=-
